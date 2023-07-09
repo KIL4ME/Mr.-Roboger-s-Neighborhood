725b768e27de8807afbb958c5f990724a3aa6f51
KIL4ME <pdo323@yahoo.com>
2023-07-09 07:13:29 -0700

    fixing heirarchy rules

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,15 @@
-window.addEventListener("load", function(){
 function substituteDigits(number) {
     const numberString = number.toString();
-    const substitutedString = "";
+    let substitutedString = "";
   
-    for (const i = 0; i < numberString.length; i++) {
-      const digit = numberString[i];
-      const substitute = "";
-  
-      if (digit === "1") {
-        substitute = "Beep!";
-      } else if (digit === "2") {
-        substitute = "Boop!";
-      } else if (digit === "3") {
-        substitute = "Won't you be my neighbor?";
-      } else {
-        substitute = digit;
-      }
-  
-      substitutedString += substitute;
+    if (numberString.includes("3")) {
+      substitutedString = "Won't you be my neighbor?";
+    } else if (numberString.includes("2")) {
+      substitutedString = "Boop!";
+    } else if (numberString.includes("1")) {
+      substitutedString = "Beep!";
+    } else {
+      substitutedString = numberString;
     }
   
     return substitutedString;
@@ -30,10 +22,10 @@ function substituteDigits(number) {
   
     const number = parseInt(numberInput.value);
   
-    for (const i = 0; i <= number; i++) {
+    for (let i = 0; i <= number; i++) {
       const substitutedNumber = substituteDigits(i);
       const listItem = document.createElement("li");
       listItem.textContent = substitutedNumber;
       resultElement.appendChild(listItem);
     }
-  }})
+  }
